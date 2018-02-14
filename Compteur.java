0c0d0ae034ae8596e9a07ae2492efd72e003d20b
Unknown <alexandre1arnaud@gmail.com>
2018-02-14 17:29:29 +0100

    Ajout fichiers

---
+++ Compteur.java
@@ -0,0 +1,41 @@
+
+/**
+ * Write a description of class Compteur here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Compteur
+{
+    // instance variables - replace the example below with your own
+    protected static int idObjet;
+
+    /**
+     * Constructor for objects of class Compteur
+     */
+    
+    public Compteur()
+    {
+        // initialise instance variables
+        int idObjet;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    
+    public int getId(int idObjet)
+    {
+        
+        return idObjet;
+    }
+    
+    public int getIdMax(int idObjet)
+    {
+        idObjet++;
+        return idObjet;
+    }
+}
